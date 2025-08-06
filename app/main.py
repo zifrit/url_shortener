88cf41da942d8f04ac7ecbce43d76d18046d218a
zifrit <zifrit@list.ru>
2025-08-06 19:43:18 +0300

    added func depends get short url by slug and add get info about short url by slug endpoint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, Request, HTTPException, status
+from typing import Annotated
+
+from fastapi import FastAPI, Request, HTTPException, status, Depends
 from fastapi.responses import RedirectResponse
 from schemas.short_url import ShortUrl
 
 SHORT_URL = [
     ShortUrl(
-        taget_url="http://www.example.com",
+        taget_url="http://www.google.com",
         slug="google",
     ),
     ShortUrl(
@@ -16,6 +18,21 @@ SHORT_URL = [
 app = FastAPI(
     title="Url Shortener",
 )
+
+
+def prefetch_slug_url(
+    slug: str,
+) -> ShortUrl:
+    url: ShortUrl | None = next(
+        (url for url in SHORT_URL if url.slug == slug),
+        None,
+    )
+    if url:
+        return url
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f'"{slug}" Not found.',
+    )
 
 
 @app.get("/")
@@ -40,15 +57,22 @@ def read_short_url_list(request: Request) -> list[ShortUrl]:
 
 @app.get("/r/{slug}/")
 def redirect_url(
-    slug: str,
+    url: Annotated[
+        ShortUrl,
+        Depends(prefetch_slug_url),
+    ],
 ):
-    url: ShortUrl | None = next(
-        (url for url in SHORT_URL if url.slug == slug),
-        None,
-    )
-    if url:
-        return RedirectResponse(url=url.taget_url)
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f'"{slug}" Not found.',
-    )
+    return RedirectResponse(url=url.taget_url)
+
+
+@app.get(
+    "/short-urls/{slug}/",
+    response_model=ShortUrl,
+)
+def info_short_urls(
+    url: Annotated[
+        ShortUrl,
+        Depends(prefetch_slug_url),
+    ],
+):
+    return url
