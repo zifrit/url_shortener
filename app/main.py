c628c44ca727720630c5be20482b833b49b408aa
zifrit <zifrit@list.ru>
2025-08-06 17:45:08 +0300

    added first api url

--- app/main.py
+++ app/main.py
@@ -1,5 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import requests
 
 app = FastAPI(
     title="Url Shortener",
 )
+
+
+@app.get("/")
+def read_root(
+    request: Request,
+    name: str = "World",
+) -> dict[str, str]:
+    docs_url = request.url.replace(
+        path="/",
+        query="",
+    )
+    return {
+        "message": f"Hello {name}",
+        "docs_url": str(docs_url),
+    }
