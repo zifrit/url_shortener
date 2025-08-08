62fc309599ed73c32dfd89b30cc34132190ad7ca
zifrit <zifrit@list.ru>
2025-08-08 18:14:26 +0300

    create film endpoint with form

--- app/api/v1/films/views.py
+++ app/api/v1/films/views.py
@@ -1,6 +1,8 @@
+import random
+import uuid
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, Form
 
 from api.v1.films.crud import FILMS
 from schemas import Films
@@ -9,11 +11,34 @@ from services.dependencies.films import prefetch_film
 router = APIRouter(prefix="/films")
 
 
-@router.get("/films/", response_model=list[Films])
+@router.get("/", response_model=list[Films])
 def read_film_list() -> list[Films]:
     return FILMS
 
 
-@router.get("/films/{film_id}/", response_model=Films)
+@router.get("/{film_id}/", response_model=Films)
 def read_film_list(film: Annotated[Films, Depends(prefetch_film)]) -> Films:
     return film
+
+
+@router.post("/", response_model=Films, status_code=status.HTTP_201_CREATED)
+def create_film(
+    name: Annotated[
+        str,
+        Form(),
+    ],
+    description: Annotated[
+        str,
+        Form(),
+    ],
+    author: Annotated[
+        str,
+        Form(),
+    ],
+):
+    return Films(
+        film_id=random.randint(1, 100),
+        name=name,
+        description=description,
+        author=author,
+    )
