4aea53d74b493d08723fec853d24f8bba75657d1
zifrit <zifrit@list.ru>
2025-08-20 13:36:57 +0300

    modified get token new it's getting from Authorise Brear

--- app/services/dependencies/other.py
+++ app/services/dependencies/other.py
@@ -1,21 +1,34 @@
 from typing import Annotated
 
-from fastapi import Header, status, HTTPException, Request
+from fastapi import Depends, status, HTTPException, Request
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from core.config import API_TOKENS
 from services.dependencies.url_shortener import UNSAFE_METHODS
+
+
+security = HTTPBearer(
+    scheme_name="Static API Token",
+    description="Your static API token from the developer portal",
+    auto_error=False,
+)
 
 
 def api_token_validate(
     request: Request,
     token: Annotated[
-        str,
-        Header(alias="x-auth-token"),
-    ] = "",
+        HTTPAuthorizationCredentials | None,
+        Depends(security),
+    ] = None,
 ):
 
     if request.method not in UNSAFE_METHODS:
         return
-    if token not in API_TOKENS:
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="API token is required",
+        )
+    if token.credentials not in API_TOKENS:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid token",
