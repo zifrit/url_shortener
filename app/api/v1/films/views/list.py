63fd176973620a831e388460596067c0af3a4980
zifrit <zifrit@list.ru>
2025-08-21 20:02:57 +0300

    added checking duplicate by slug when create films or short url

--- app/api/v1/films/views/list.py
+++ app/api/v1/films/views/list.py
@@ -1,4 +1,4 @@
-from fastapi import status, Depends
+from fastapi import status, HTTPException
 
 from api.v1.films.crud import film_storage
 from schemas import Films, FilmsCreate, FilmsRead
@@ -15,8 +15,27 @@ def read_film_list() -> list[Films]:
     return film_storage.get()
 
 
-@router.post("/", response_model=FilmsRead, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/",
+    response_model=FilmsRead,
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        status.HTTP_409_CONFLICT: {
+            "description": "Invalid request",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Film with slug='foobar' already exists."},
+                },
+            },
+        },
+    },
+)
 def create_film(
     film: FilmsCreate,
 ):
-    return film_storage.create(film)
+    if not film_storage.get_by_slug(film.slug):
+        return film_storage.create(film)
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=f"Film with this slug={film.slug!r} already exists",
+    )
--- app/api/v1/url_shortener/views/list.py
+++ app/api/v1/url_shortener/views/list.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Request, status, Body, BackgroundTasks, Depends
+from fastapi import APIRouter, Request, status, Body, HTTPException
 from api.v1.url_shortener.crud import storage
 from schemas import ShortUrl, ShortUrlCreate, ShortUrlRead
 
@@ -17,8 +17,25 @@ def read_short_url_list(request: Request) -> list[ShortUrl]:
     "/",
     response_model=ShortUrlRead,
     status_code=status.HTTP_201_CREATED,
+    responses={
+        status.HTTP_409_CONFLICT: {
+            "description": "Invalid request",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "Short URL with slug='foobar' already exists."
+                    },
+                },
+            },
+        },
+    },
 )
 def create_short_url(
     short_url_create: Annotated[ShortUrlCreate, Body()],
 ) -> ShortUrl:
-    return storage.create(short_url_create)
+    if not storage.get_by_slug(short_url_create.slug):
+        return storage.create(short_url_create)
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=f"Short URL with this slug={short_url_create.slug!r} already exists",
+    )
