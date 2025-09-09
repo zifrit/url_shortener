88aae9f6200f137de9e1ee5cc0606a7fccad826d
zifrit <zifrit@list.ru>
2025-09-09 16:46:56 +0300

    added test that verifies that the token exist in redis

---
+++ app/testing/test_api/test_v1/test_auth/test_services/test_redis_token_storage.py
@@ -0,0 +1,19 @@
+from os import getenv
+from unittest import TestCase
+
+from api.v1.auth.services.by_token import cache_token_storage
+
+if getenv("TESTING") != "1":
+    raise OSError(  # noqa: TRY003
+        "Environmental is not ready to start test",  # noqa: EM101
+    )
+
+
+class RedisTokenStorageTestCase(TestCase):
+    def test_generate_and_save_token(self) -> None:
+        token = cache_token_storage.generate_and_save_token()
+        expected_exist = True
+        self.assertEqual(
+            expected_exist,
+            cache_token_storage.token_exists(token),
+        )
