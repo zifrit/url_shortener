29beedf2438418ecb6351916f152cd3e6fba0e6f
zifrit <zifrit@list.ru>
2025-09-20 23:48:56 +0300

    modified root test and add new root test with parametrizes

--- app/testing/test_api/test_v1/test_views.py
+++ app/testing/test_api/test_v1/test_views.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 
@@ -7,7 +8,24 @@ client = TestClient(app)
 
 
 def test_root_view() -> None:
-    name = "Jone"
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+    response_date = response.json()
+    expectest_message = "Hello World"
+    assert "message" in response_date
+    assert response_date["message"] == expectest_message, response_date
+
+
+@pytest.mark.parametrize(
+    "name",
+    [
+        "name",
+        "Mike",
+        "",
+        "%#@",
+    ],
+)
+def test_root_view_with_custom_name(name: str) -> None:
     params = {"name": name}
     response = client.get("/", params=params)
     assert response.status_code == status.HTTP_200_OK
