7ab19931279b9f80ff0441c344c150d469152f93
zifrit <zifrit@list.ru>
2025-09-26 22:33:21 +0300

    added test crearte films\short_urls with invalid data from api methods

--- app/testing/test_api/test_v1/test_films/test_views/test_create.py
+++ app/testing/test_api/test_v1/test_films/test_views/test_create.py
@@ -1,11 +1,15 @@
 import random
 import string
+from typing import Any
 
+import pytest
+from _pytest.fixtures import SubRequest
 from fastapi import status
 from fastapi.testclient import TestClient
 
 from main import app
 from schemas import Films, FilmsCreate
+from testing.test_api.conftest import create_films_random_slug
 
 
 def test_create_film(auth_client: TestClient) -> None:
@@ -33,3 +37,33 @@ def test_create_film_already_exists(auth_client: TestClient, film: Films) -> Non
         response_data["detail"]
         == f"Film with this slug='{data["slug"]}' already exists"
     )
+
+
+class TestCreateInvalid:
+
+    @pytest.fixture(
+        params=[
+            pytest.param(("s", "string_too_short"), id="short_Slug"),
+            pytest.param(("some-long-slug-text", "string_too_long"), id="short_Slug"),
+        ],
+    )
+    def films_create_values(self, request: SubRequest) -> tuple[dict[str, Any], str]:
+        build = create_films_random_slug()
+        data = build.model_dump(mode="json")
+        slug, error = request.param
+        data["slug"] = slug
+        return data, error
+
+    def test_invalid_slug(
+        self,
+        films_create_values: tuple[dict[str, Any], str],
+        auth_client: TestClient,
+    ) -> None:
+        url = app.url_path_for("create_film")
+        create_data, error = films_create_values
+        response = auth_client.post(url=url, json=create_data)
+        assert (
+            response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+        ), response.text
+        error_details = response.json()["detail"][0]
+        assert error_details["type"] == error
--- app/testing/test_api/test_v1/test_url_shortener/test_views/test_create.py
+++ app/testing/test_api/test_v1/test_url_shortener/test_views/test_create.py
@@ -1,11 +1,15 @@
 import random
 import string
+from typing import Any
 
+import pytest
+from _pytest.fixtures import SubRequest
 from fastapi import status
 from fastapi.testclient import TestClient
 
 from main import app
 from schemas import ShortUrl, ShortUrlCreate
+from testing.test_api.conftest import create_short_url_random_slug
 
 
 def test_create_short_ulr(auth_client: TestClient) -> None:
@@ -36,3 +40,36 @@ def test_create_short_ulr_already_exist(
         response_data["detail"]
         == f"Short URL with this slug='{data["slug"]}' already exists"
     )
+
+
+class TestCreateInvalid:
+
+    @pytest.fixture(
+        params=[
+            pytest.param(("s", "string_too_short"), id="short_Slug"),
+            pytest.param(("some-long-slug-text", "string_too_long"), id="short_Slug"),
+        ],
+    )
+    def short_url_create_values(
+        self,
+        request: SubRequest,
+    ) -> tuple[dict[str, Any], str]:
+        build = create_short_url_random_slug()
+        data = build.model_dump(mode="json")
+        slug, error = request.param
+        data["slug"] = slug
+        return data, error
+
+    def test_invalid_slug(
+        self,
+        short_url_create_values: tuple[dict[str, Any], str],
+        auth_client: TestClient,
+    ) -> None:
+        url = app.url_path_for("create_short_url")
+        create_data, error = short_url_create_values
+        response = auth_client.post(url=url, json=create_data)
+        assert (
+            response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+        ), response.text
+        error_details = response.json()["detail"][0]
+        assert error_details["type"] == error
