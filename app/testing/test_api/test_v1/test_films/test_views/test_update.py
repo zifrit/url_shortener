71859185b1c31b6b582753e98a989ba28403772a
zifrit <zifrit@list.ru>
2025-09-29 21:48:44 +0300

    added new test. full update films\short_urls

---
+++ app/testing/test_api/test_v1/test_films/test_views/test_update.py
@@ -0,0 +1,83 @@
+from collections.abc import Generator
+
+import pytest
+from _pytest.fixtures import SubRequest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from api.v1.films.crud import film_storage
+from main import app
+from schemas import Films, FilmsUpdate
+from testing.test_api.conftest import create_films_random_slug
+
+
+class TestUpdate:
+
+    @pytest.fixture()
+    def film(self, request: SubRequest) -> Generator[Films]:
+        description, name, author = request.param
+        film = create_films_random_slug(
+            description=description,
+            name=name,
+            author=author,
+        )
+        yield film
+        film_storage.delete(film)
+
+    @pytest.mark.parametrize(
+        "film, new_description, new_name, new_author",
+        [
+            pytest.param(
+                ("description", "name", "author"),
+                "new_description",
+                "new_name",
+                "new_author",
+                id="standard",
+            ),
+            pytest.param(
+                ("description", "name", "author"),
+                "new_description",
+                "n",
+                "new_author",
+                id="standard",
+            ),
+            pytest.param(
+                ("description", "name", "author"),
+                "new_description",
+                "new_name",
+                "",
+                id="standard",
+            ),
+        ],
+        indirect=["film"],
+    )
+    def test_update_film_details(
+        self,
+        film: Films,
+        new_description: str,
+        new_name: str,
+        new_author: str,
+        auth_client: TestClient,
+    ) -> None:
+        url = app.url_path_for(
+            "update_film_details",
+            slug=film.slug,
+        )
+        data = FilmsUpdate(
+            description=new_description,
+            name=new_name,
+            author=new_author,
+        )
+        old_description = film.description
+        old_name = film.name
+        old_author = film.author
+        response = auth_client.patch(url, json=data.model_dump(mode="json"))
+        assert response.status_code == status.HTTP_200_OK, response.text
+        film_db = film_storage.get_by_slug(film.slug)
+        assert film_db
+        assert old_description != new_description
+        assert film_db.description == new_description
+        assert old_name != new_name
+        assert film_db.name == new_name
+        assert old_author != new_author
+        assert film_db.author == new_author
---
+++ app/testing/test_api/test_v1/test_url_shortener/test_views/test_update.py
@@ -0,0 +1,72 @@
+from collections.abc import Generator
+
+import pytest
+from _pytest.fixtures import SubRequest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from api.v1.url_shortener.crud import storage
+from main import app
+from schemas import ShortUrl, ShortUrlUpdate
+from testing.test_api.conftest import create_short_url
+
+
+class TestUpdate:
+
+    @pytest.fixture()
+    def short_url(self, request: SubRequest) -> Generator[ShortUrl]:
+        slug, description, target_url = request.param
+        short_url = create_short_url(
+            target_url=target_url,
+            description=description,
+            slug=slug,
+        )
+        yield short_url
+        storage.delete(short_url)
+
+    @pytest.mark.parametrize(
+        "short_url, new_description, new_target_url",
+        [
+            pytest.param(
+                ("new_slug_1", "description", "https://example.com/"),
+                "new_description",
+                "https://new-example.com/",
+                id="new_slug_1",
+            ),
+            pytest.param(
+                ("new_slug_2", "", "https://google.com/"),
+                "new_description",
+                "https://example.com/",
+                id="new_slug_2",
+            ),
+            pytest.param(
+                ("new_slug_3", "description", "http://ya.com/"),
+                "",
+                "http://new.com/",
+                id="new_slug_3",
+            ),
+        ],
+        indirect=["short_url"],
+    )
+    def test_update_short_url(
+        self,
+        short_url: ShortUrl,
+        new_description: str,
+        new_target_url: str,
+        auth_client: TestClient,
+    ) -> None:
+        url = app.url_path_for("update_short_url_details", slug=short_url.slug)
+        data = ShortUrlUpdate(
+            description=new_description,
+            target_url=new_target_url,
+        )
+        old_description = short_url.description
+        old_target_url = short_url.target_url
+        response = auth_client.put(url, json=data.model_dump(mode="json"))
+        assert response.status_code == status.HTTP_200_OK, response.text
+        short_url_db = storage.get_by_slug(short_url.slug)
+        assert short_url_db
+        assert short_url_db.description == new_description
+        assert str(short_url_db.target_url) == new_target_url
+        assert old_description != new_description
+        assert str(old_target_url) != new_target_url
