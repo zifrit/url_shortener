de643e1db4678f0adc6349a023a714bf1381e76a
zifrit <zifrit@list.ru>
2025-11-02 16:57:29 +0300

    added pars env from yaml file

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,10 @@ from typing import Literal
 
 from pydantic import BaseModel, model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings.sources import (
+    PydanticBaseSettingsSource,
+    YamlConfigSettingsSource,
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,9 +79,45 @@ class Settings(BaseSettings):
         ),
         env_nested_delimiter="__",
         env_prefix="ENV__",
+        yaml_file=(
+            BASE_DIR / "config.default.yaml",
+            "",
+        ),
+        yaml_config_section="app_config",
     )
     logging: LoggingConfig = LoggingConfig()
     redis: RedisConfig = RedisConfig()
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        """
+        Define the sources and their order for loading the settings values.
+
+        Args:
+            settings_cls: The Settings class.
+            init_settings: The `InitSettingsSource` instance.
+            env_settings: The `EnvSettingsSource` instance.
+            dotenv_settings: The `DotEnvSettingsSource` instance.
+            file_secret_settings: The `SecretsSettingsSource` instance.
+
+        Returns:
+            A tuple containing the sources and their
+            order for loading the settings values.
+        """
+        return (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            file_secret_settings,
+            YamlConfigSettingsSource(settings_cls),
+        )
 
 
 settings = Settings()
