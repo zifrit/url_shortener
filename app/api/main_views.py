20a53824c018e7adb88bb52b049ab85d6771df2f
zifrit <zifrit@list.ru>
2026-01-15 18:35:38 +0300

    modified root endpoint

--- app/api/main_views.py
+++ app/api/main_views.py
@@ -9,6 +9,7 @@ router = APIRouter()
 @router.get(
     "/",
     response_class=HTMLResponse,
+    include_in_schema=False,
 )
 def read_root() -> str:
     return (BASE_DIR / "frontend" / "home.html").read_text()
