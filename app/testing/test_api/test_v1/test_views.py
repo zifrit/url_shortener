5628fbcf31a072473c4f0ec630c801e5a7b1ecb7
zifrit <zifrit@list.ru>
2026-01-25 16:13:05 +0300

    update root view tests to check template and context features

--- app/testing/test_api/test_v1/test_views.py
+++ app/testing/test_api/test_v1/test_views.py
@@ -6,29 +6,5 @@ from fastapi.testclient import TestClient
 def test_root_view(client: TestClient) -> None:
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
-    response_date = response.json()
-    expectest_message = "Hello World"
-    assert "message" in response_date
-    assert response_date["message"] == expectest_message, response_date
-
-
-@pytest.mark.parametrize(
-    "name",
-    [
-        "name",
-        "Mike",
-        "",
-        "%#@",
-    ],
-)
-def test_root_view_with_custom_name(
-    name: str,
-    client: TestClient,
-) -> None:
-    params = {"name": name}
-    response = client.get("/", params=params)
-    assert response.status_code == status.HTTP_200_OK
-    response_date = response.json()
-    expectest_message = f"Hello {name}"
-    assert "message" in response_date
-    assert response_date["message"] == expectest_message, response_date
+    assert response.template.name == "home.html"
+    assert "features" in response.context
