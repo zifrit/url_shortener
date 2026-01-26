a84e750419924c45c674923e431448c314c6af38
zifrit <zifrit@list.ru>
2026-01-26 23:53:35 +0300

    refactor: update film and short_url storage dependencies for improved modularity

--- app/services/dependencies/films.py
+++ app/services/dependencies/films.py
@@ -1,19 +1,29 @@
 import logging
+from typing import Annotated
 
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
 from starlette import status
 
-from storage.film.crud import film_storage
+from core import config
+from storage.film.crud import FilmStorage
 from schemas import Films
 
 log = logging.getLogger(__name__)
 
 
+def get_film_storage() -> FilmStorage:
+    return FilmStorage(hash_name=config.settings.redis.token.film)
+
+
+GetFilmStorage = Annotated[FilmStorage, Depends(get_film_storage)]
+
+
 def prefetch_film(
     slug: str,
+    storage: FilmStorage = Depends(get_film_storage),
 ) -> Films:
 
-    if film := film_storage.get_by_slug(slug=slug):
+    if film := storage.get_by_slug(slug=slug):
         return film
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
--- app/services/dependencies/url_shortener.py
+++ app/services/dependencies/url_shortener.py
@@ -1,16 +1,26 @@
 import logging
+from typing import Annotated
 
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
 from starlette import status
 
-from storage.short_ulr.crud import storage
+from core import config
+from storage.short_ulr.crud import ShortUrlStorage
 from schemas import ShortUrl
 
 log = logging.getLogger(__name__)
 
 
+def get_short_url_storage() -> ShortUrlStorage:
+    return ShortUrlStorage(hash_name=config.settings.redis.token.short_url)
+
+
+GetShortUrlStorage = Annotated[ShortUrlStorage, Depends(get_short_url_storage)]
+
+
 def prefetch_slug_url(
     slug: str,
+    storage: ShortUrlStorage = Depends(get_short_url_storage),
 ) -> ShortUrl:
     url: ShortUrl | None = storage.get_by_slug(slug=slug)
     if url:
