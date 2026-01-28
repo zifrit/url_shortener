27f2d8f1ea4db777754529b8f9ed0f8c3e9c60ae
zifrit <zifrit@list.ru>
2026-01-28 22:15:32 +0300

    add: implement listing of short URLs in the web interface

--- app/web/short_url/list.py
+++ app/web/short_url/list.py
@@ -1,15 +1,23 @@
+from typing import Any
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 
 from api.jinja_temp import templates
+from services.dependencies.url_shortener import GetShortUrlStorage
 
 router = APIRouter()
 
 
 @router.get("/list", name="short_url:list")
-def list_short_urls(request: Request) -> HTMLResponse:
+def list_short_urls(
+    request: Request,
+    storage: GetShortUrlStorage,
+) -> HTMLResponse:
+    context: dict[str, Any] = {}
+    context.update(list_of_short_urls=storage.get())
     return templates.TemplateResponse(
         request=request,
         name="short_url/list.html",
-        context={},
+        context=context,
     )
