335961c2e55364fa91c1736ea6d49b5203c6a5e3
zifrit <zifrit@list.ru>
2026-01-29 20:41:18 +0300

    add: implement listing of films in the web interface

--- app/web/film/list.py
+++ app/web/film/list.py
@@ -1,15 +1,23 @@
+from typing import Any
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 
 from api.jinja_temp import templates
+from services.dependencies.films import GetFilmStorage
 
 router = APIRouter()
 
 
 @router.get("/list", name="film:list")
-def list_film(request: Request) -> HTMLResponse:
+def list_film(
+    request: Request,
+    storage: GetFilmStorage,
+) -> HTMLResponse:
+    context: dict[str, Any] = {}
+    context.update(list_of_films=storage.get())
     return templates.TemplateResponse(
         request=request,
         name="film/list.html",
-        context={},
+        context=context,
     )
