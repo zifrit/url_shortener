fca5fb871d881ecfa2166bce5172ccae808da8f8
zifrit <zifrit@list.ru>
2026-02-03 21:34:25 +0300

    feat: add backend validate form and view it into web page

--- app/web/film/details.py
+++ app/web/film/details.py
@@ -5,6 +5,7 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from api.jinja_temp import templates
 from schemas.films import FilmsCreate
 from services.dependencies.films import GetFilmStorage
+from storage.film.exception import AlreadyExistFilmError
 
 router = APIRouter(prefix="/create")
 
@@ -15,11 +16,27 @@ def create_film(
     storage: GetFilmStorage,
     request: Request,
 ) -> RedirectResponse:
-    storage.create_or_raise_if_exists(data)
-    return RedirectResponse(
-        url=request.url_for("film:list"),
-        status_code=status.HTTP_303_SEE_OTHER,
-    )
+    try:
+        storage.create_or_raise_if_exists(data)
+        return RedirectResponse(
+            url=request.url_for("film:list"),
+            status_code=status.HTTP_303_SEE_OTHER,
+        )
+    except AlreadyExistFilmError:
+        error = {"slug": f"Film with this slug={data.slug!r} already exists"}
+        context: dict[str, Any] = {}
+        context.update(
+            create_schema=FilmsCreate.model_json_schema(),
+            error=error,
+            validated=True,
+            from_data=data,
+        )
+        return templates.TemplateResponse(
+            request=request,
+            name="film/create.html",
+            context=context,
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        )
 
 
 @router.get("/", name="film:create-view")
--- app/web/short_url/details.py
+++ app/web/short_url/details.py
@@ -5,21 +5,38 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from api.jinja_temp import templates
 from schemas.short_url import ShortUrlCreate
 from services.dependencies.url_shortener import GetShortUrlStorage
+from storage.short_ulr.exceptions import AlreadyExistsShortUrlError
 
 router = APIRouter(prefix="/create")
 
 
-@router.post("/", name="short_url:create")
+@router.post("/", name="short_url:create", response_model=None)
 def create_short_url(
     data: Annotated[ShortUrlCreate, Form()],
     storage: GetShortUrlStorage,
     request: Request,
-) -> RedirectResponse:
-    storage.create_or_raise_if_exists(data)
-    return RedirectResponse(
-        url=request.url_for("short_url:list"),
-        status_code=status.HTTP_303_SEE_OTHER,
-    )
+) -> RedirectResponse | HTMLResponse:
+    try:
+        storage.create_or_raise_if_exists(data)
+        return RedirectResponse(
+            url=request.url_for("short_url:list"),
+            status_code=status.HTTP_303_SEE_OTHER,
+        )
+    except AlreadyExistsShortUrlError:
+        error = {"slug": f"Short URL with this slug={data.slug!r} already exists"}
+        context: dict[str, Any] = {}
+        context.update(
+            create_schema=ShortUrlCreate.model_json_schema(),
+            error=error,
+            validated=True,
+            from_data=data,
+        )
+        return templates.TemplateResponse(
+            request=request,
+            name="short_url/create.html",
+            context=context,
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        )
 
 
 @router.get("/", name="short_url:create-view")
