b1e134ca2704e9ffa9909b65ac49006efd177599
zifrit <zifrit@list.ru>
2026-02-04 22:04:16 +0300

    feat: add from validate

--- app/web/film/details.py
+++ app/web/film/details.py
@@ -1,7 +1,9 @@
-from typing import Annotated, Any
+from typing import Annotated, Any, Mapping
 
 from fastapi import APIRouter, Form, Request, status
 from fastapi.responses import HTMLResponse, RedirectResponse
+from pydantic import ValidationError
+
 from api.jinja_temp import templates
 from schemas.films import FilmsCreate
 from services.dependencies.films import GetFilmStorage
@@ -10,12 +12,45 @@ from storage.film.exception import AlreadyExistFilmError
 router = APIRouter(prefix="/create")
 
 
-@router.post("/", name="film:create")
-def create_film(
-    data: Annotated[FilmsCreate, Form()],
+def create_view_validate_response(
+    errors: dict[str, str],
+    data: FilmsCreate | Mapping[str, Any],
+    request: Request,
+) -> HTMLResponse:
+    context: dict[str, Any] = {}
+    context.update(
+        create_schema=FilmsCreate.model_json_schema(),
+        error=errors,
+        validated=True,
+        from_data=data,
+    )
+    return templates.TemplateResponse(
+        request=request,
+        name="film/create.html",
+        context=context,
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    )
+
+
+def format_pydantic_errors(errors: list[dict[str, Any]]) -> dict[str, str]:
+    return {error["loc"][0]: error["msg"] for error in errors}
+
+
+@router.post("/", name="film:create", response_model=None)
+async def create_film(
     storage: GetFilmStorage,
     request: Request,
-) -> RedirectResponse:
+) -> RedirectResponse | HTMLResponse:
+    async with request.form() as form:
+        try:
+            data = FilmsCreate.model_validate(form)
+        except ValidationError as error:
+            errors = format_pydantic_errors(error.errors())
+            return create_view_validate_response(
+                errors=errors,
+                data=form,
+                request=request,
+            )
     try:
         storage.create_or_raise_if_exists(data)
         return RedirectResponse(
@@ -23,19 +58,11 @@ def create_film(
             status_code=status.HTTP_303_SEE_OTHER,
         )
     except AlreadyExistFilmError:
-        error = {"slug": f"Film with this slug={data.slug!r} already exists"}
-        context: dict[str, Any] = {}
-        context.update(
-            create_schema=FilmsCreate.model_json_schema(),
-            error=error,
-            validated=True,
-            from_data=data,
-        )
-        return templates.TemplateResponse(
+        errors = {"slug": f"Film with this slug={data.slug!r} already exists"}
+        return create_view_validate_response(
+            errors=errors,
+            data=data,
             request=request,
-            name="film/create.html",
-            context=context,
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         )
 
 
--- app/web/short_url/details.py
+++ app/web/short_url/details.py
@@ -1,7 +1,9 @@
-from typing import Annotated, Any
+from typing import Annotated, Any, Mapping
 
 from fastapi import APIRouter, Form, Request, status
 from fastapi.responses import HTMLResponse, RedirectResponse
+from pydantic import ValidationError
+
 from api.jinja_temp import templates
 from schemas.short_url import ShortUrlCreate
 from services.dependencies.url_shortener import GetShortUrlStorage
@@ -10,12 +12,45 @@ from storage.short_ulr.exceptions import AlreadyExistsShortUrlError
 router = APIRouter(prefix="/create")
 
 
+def create_view_validate_response(
+    errors: dict[str, str],
+    data: ShortUrlCreate | Mapping[str, Any],
+    request: Request,
+) -> HTMLResponse:
+    context: dict[str, Any] = {}
+    context.update(
+        create_schema=ShortUrlCreate.model_json_schema(),
+        error=errors,
+        validated=True,
+        from_data=data,
+    )
+    return templates.TemplateResponse(
+        request=request,
+        name="short_url/create.html",
+        context=context,
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    )
+
+
+def format_pydantic_errors(errors: list[dict[str, Any]]) -> dict[str, str]:
+    return {error["loc"][0]: error["msg"] for error in errors}
+
+
 @router.post("/", name="short_url:create", response_model=None)
-def create_short_url(
-    data: Annotated[ShortUrlCreate, Form()],
+async def create_short_url(
     storage: GetShortUrlStorage,
     request: Request,
 ) -> RedirectResponse | HTMLResponse:
+    async with request.form() as form:
+        try:
+            data = ShortUrlCreate.model_validate(form)
+        except ValidationError as error:
+            errors = format_pydantic_errors(error.errors())
+            return create_view_validate_response(
+                errors=errors,
+                data=form,
+                request=request,
+            )
     try:
         storage.create_or_raise_if_exists(data)
         return RedirectResponse(
@@ -23,19 +58,11 @@ def create_short_url(
             status_code=status.HTTP_303_SEE_OTHER,
         )
     except AlreadyExistsShortUrlError:
-        error = {"slug": f"Short URL with this slug={data.slug!r} already exists"}
-        context: dict[str, Any] = {}
-        context.update(
-            create_schema=ShortUrlCreate.model_json_schema(),
-            error=error,
-            validated=True,
-            from_data=data,
-        )
-        return templates.TemplateResponse(
+        errors = {"slug": f"Short URL with this slug={data.slug!r} already exists"}
+        return create_view_validate_response(
+            errors=errors,
+            data=data,
             request=request,
-            name="short_url/create.html",
-            context=context,
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         )
 
 
