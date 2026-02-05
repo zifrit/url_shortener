ca6f25eeb5b893f7ece4ff8ba853a7ee88178d9f
zifrit <zifrit@list.ru>
2026-02-05 20:36:11 +0300

    add basic auth validate in web

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from services.dependencies.other import username_password_auth
 from web.main_views import router as main_router
 from web.short_url import router as short_url_router
 from web.film import router as film_router
 
-router = APIRouter()
+router = APIRouter(dependencies=[Depends(username_password_auth)])
 router.include_router(main_router)
 router.include_router(short_url_router)
 router.include_router(film_router)
